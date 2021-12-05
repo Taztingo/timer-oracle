37f554f874ad12c9d72607f67c06ca0fc617d3c1
Matthew Witkowski <taztingo503@gmail.com>
2021-12-05 16:39:11 -0500

    Completed some tests.

--- test/heartbeat.js
+++ test/heartbeat.js
@@ -2,39 +2,80 @@ const { expect, assert } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Heartbeat", () => {
-  before(async () => {
-    const TimerManager = await ethers.getContractFactory("TimerManager");
+  /*before(async () => {
+    const TimerManager = await ethers.getContractFactory("MockTimerManager");
     const timerManager = await TimerManager.deploy();
     await timerManager.deployed();
 
     const Heartbeat = await ethers.getContractFactory("Heartbeat");
     const heartbeat = await Heartbeat.deploy(timerManager.address);
     await heartbeat.deployed();
-  });
+  });*/
 
-  it("Should initially set the address for the timer manager", async function () {
+  it("Constructor should initially set the address for the timer manager", async function () {
+    /*const TimerManager = await ethers.getContractFactory("MockTimerManager");
+    const timerManager = await TimerManager.deploy();
+    await timerManager.deployed();
+
+    const Heartbeat = await ethers.getContractFactory("Heartbeat");
+    const heartbeat = await Heartbeat.deploy(timerManager.address);
+    await heartbeat.deployed();*/
     assert(false, "Not yet implemented!");
   });
 
-  it("Should update messages with add", async function() {
-    //const tx = await heartbeat.add();
-    //await tx.wait();
-    assert(false, "Not yet implemented!");
+  it("add should create a new heartbeat", async function() {
+    const TimerManager = await ethers.getContractFactory("MockTimerManager");
+    const timerManager = await TimerManager.deploy();
+    await timerManager.deployed();
+
+    const Heartbeat = await ethers.getContractFactory("Heartbeat");
+    const heartbeat = await Heartbeat.deploy(timerManager.address);
+    await heartbeat.deployed();
+
+    await expect(heartbeat.add("Hello World", 5, false))
+      .to.emit(heartbeat, "HeartbeatAddEvent")
+      .withArgs(0, "Hello World");
   });
 
-  it("Should update the address for the timer manager", async function() {
-    assert(false, "Not yet implemented!");
+  it("setTimerManager should update the address for the timer manager", async function() {
+    const TimerManager = await ethers.getContractFactory("MockTimerManager");
+    const timerManager = await TimerManager.deploy();
+    await timerManager.deployed();
+
+    const Heartbeat = await ethers.getContractFactory("Heartbeat");
+    const heartbeat = await Heartbeat.deploy(timerManager.address);
+    await heartbeat.deployed();
+
+    await expect(heartbeat.setTimerManagerAddress(heartbeat.address))
+      .to.emit(heartbeat, "TimerManagerUpdateEvent")
+      .withArgs(heartbeat.address);
   });
 
-  it("Should emit HeartbeatEvent on timeout", async function() {
-    assert(false, "Not yet implemented!");
+  it("onTimeout should emit HeartbeatEvent on success", async function() {
+    const TimerManager = await ethers.getContractFactory("MockTimerManager");
+    const timerManager = await TimerManager.deploy();
+    await timerManager.deployed();
+
+    const Heartbeat = await ethers.getContractFactory("Heartbeat");
+    const heartbeat = await Heartbeat.deploy(timerManager.address);
+    await heartbeat.deployed();
+
+    await heartbeat.add("Hello World", 5, false);
+    await expect(heartbeat.onTimeout(0))
+      .to.emit(heartbeat, "HeartbeatEvent")
+      .withArgs("Hello World");
   });
 
-  it("Should rollback if timer id does not exist", async function() {
-    assert(false, "Not yet implemented!");
-  });
+  it("onTimeout should rollback if timer id does not exist", async function() {
+    const TimerManager = await ethers.getContractFactory("MockTimerManager");
+    const timerManager = await TimerManager.deploy();
+    await timerManager.deployed();
 
-  it("Should delete id from messages on timeout", async function() {
-    assert(false, "Not yet implemented!");
+    const Heartbeat = await ethers.getContractFactory("Heartbeat");
+    const heartbeat = await Heartbeat.deploy(timerManager.address);
+    await heartbeat.deployed();
+
+    await heartbeat.add("Hello World", 5, false);
+    await expect(heartbeat.onTimeout(1)).to.be.reverted;
   });
 });
