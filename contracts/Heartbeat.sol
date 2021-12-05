cc4e268f6de1e9d8e2b73d0e56d1d2b9b42a330f
Matthew Witkowski <taztingo503@gmail.com>
2021-12-05 16:39:01 -0500

    Added additional events to help with testing. Created a MockTimerManager to make testing easier.

--- contracts/Heartbeat.sol
+++ contracts/Heartbeat.sol
@@ -8,25 +8,29 @@ import "./ITimer.sol";
 contract Heartbeat is ITimerCallback {
     constructor(address _address) {
         timer = ITimer(_address);
+        emit TimerManagerUpdateEvent(_address);
     }
 
-    event HeartbeatEvent(string _memory);
+    event HeartbeatEvent(string _message);
+    event HeartbeatAddEvent(uint _id, string _message);
+    event TimerManagerUpdateEvent(address _address);
     mapping(uint => string) messages;
-
     ITimer timer;
 
     modifier hasId(uint _id) {
-        require(keccak256(abi.encodePacked(messages[_id])) == keccak256(abi.encodePacked("")) , "The id does not exist");
+        require(keccak256(abi.encodePacked(messages[_id])) != keccak256(abi.encodePacked("")) , "The id does not exist");
         _;
     }
 
     function add(string calldata _message, uint _seconds, bool _periodic) external {
         uint id = timer.start(_seconds, _periodic);
         messages[id] = _message;
+        emit HeartbeatAddEvent(id, _message);
     }
 
     function setTimerManagerAddress(address _address) external {
         timer = ITimer(_address);
+        emit TimerManagerUpdateEvent(_address);
     }
 
     function onTimeout(uint _id) external override hasId(_id) {
