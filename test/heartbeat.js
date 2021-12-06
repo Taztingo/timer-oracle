990138492e77f028836956568a81b5bcb86dd97e
Matthew Witkowski <taztingo503@gmail.com>
2021-12-05 23:02:43 -0500

    Finished tests with a mock contract. Looking into how to remove the mock contract.

--- test/heartbeat.js
+++ test/heartbeat.js
@@ -2,80 +2,40 @@ const { expect, assert } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Heartbeat", () => {
-  /*before(async () => {
-    const TimerManager = await ethers.getContractFactory("MockTimerManager");
-    const timerManager = await TimerManager.deploy();
+  let TimerManager;
+  let timerManager;
+  let Heartbeat;
+  let heartbeat;
+
+  before(async () => {
+    TimerManager = await ethers.getContractFactory("MockTimerManager");
+    timerManager = await TimerManager.deploy();
     await timerManager.deployed();
 
-    const Heartbeat = await ethers.getContractFactory("Heartbeat");
-    const heartbeat = await Heartbeat.deploy(timerManager.address);
+    Heartbeat = await ethers.getContractFactory("Heartbeat");
+    heartbeat = await Heartbeat.deploy(timerManager.address);
     await heartbeat.deployed();
-  });*/
-
-  it("Constructor should initially set the address for the timer manager", async function () {
-    /*const TimerManager = await ethers.getContractFactory("MockTimerManager");
-    const timerManager = await TimerManager.deploy();
-    await timerManager.deployed();
-
-    const Heartbeat = await ethers.getContractFactory("Heartbeat");
-    const heartbeat = await Heartbeat.deploy(timerManager.address);
-    await heartbeat.deployed();*/
-    assert(false, "Not yet implemented!");
   });
 
   it("add should create a new heartbeat", async function() {
-    const TimerManager = await ethers.getContractFactory("MockTimerManager");
-    const timerManager = await TimerManager.deploy();
-    await timerManager.deployed();
-
-    const Heartbeat = await ethers.getContractFactory("Heartbeat");
-    const heartbeat = await Heartbeat.deploy(timerManager.address);
-    await heartbeat.deployed();
-
     await expect(heartbeat.add("Hello World", 5, false))
       .to.emit(heartbeat, "HeartbeatAddEvent")
       .withArgs(0, "Hello World");
   });
 
   it("setTimerManager should update the address for the timer manager", async function() {
-    const TimerManager = await ethers.getContractFactory("MockTimerManager");
-    const timerManager = await TimerManager.deploy();
-    await timerManager.deployed();
-
-    const Heartbeat = await ethers.getContractFactory("Heartbeat");
-    const heartbeat = await Heartbeat.deploy(timerManager.address);
-    await heartbeat.deployed();
-
     await expect(heartbeat.setTimerManagerAddress(heartbeat.address))
       .to.emit(heartbeat, "TimerManagerUpdateEvent")
       .withArgs(heartbeat.address);
   });
 
   it("onTimeout should emit HeartbeatEvent on success", async function() {
-    const TimerManager = await ethers.getContractFactory("MockTimerManager");
-    const timerManager = await TimerManager.deploy();
-    await timerManager.deployed();
-
-    const Heartbeat = await ethers.getContractFactory("Heartbeat");
-    const heartbeat = await Heartbeat.deploy(timerManager.address);
-    await heartbeat.deployed();
-
-    await heartbeat.add("Hello World", 5, false);
     await expect(heartbeat.onTimeout(0))
       .to.emit(heartbeat, "HeartbeatEvent")
       .withArgs("Hello World");
   });
 
   it("onTimeout should rollback if timer id does not exist", async function() {
-    const TimerManager = await ethers.getContractFactory("MockTimerManager");
-    const timerManager = await TimerManager.deploy();
-    await timerManager.deployed();
-
-    const Heartbeat = await ethers.getContractFactory("Heartbeat");
-    const heartbeat = await Heartbeat.deploy(timerManager.address);
-    await heartbeat.deployed();
-
-    await heartbeat.add("Hello World", 5, false);
     await expect(heartbeat.onTimeout(1)).to.be.reverted;
   });
 });
